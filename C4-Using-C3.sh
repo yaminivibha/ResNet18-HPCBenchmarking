f4913e1ba1ef866ce7c4efb4ca5d7a16110bb01c
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-02-25 21:59:15 -0500

    C4 script

---
+++ C4-Using-C3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Simple "Hello World" submit script for Slurm.
+#
+#SBATCH --account=edu               # Replace ACCOUNT with your group account name
+#SBATCH --job-name=HW2-C1           # The job name.
+#SBATCH -c 3                        # The number of cpu cores to use
+#SBATCH -t 0-00:30                  # Runtime in D-HH:MM
+#SBATCH --mem-per-cpu=10gb          # The memory the job will use per cpu core
+#SBATCH --mail-user=yva2002@columbia.edu
+
+
+module load anaconda
+
+conda init bash
+
+source ~/.bashrc
+
+conda activate hw2
+
+python3 lab2.py C2 --dataloader_workers 1 --outfile C4-1worker.txt
+python3 lab2.py C2 --dataloader_workers 4 --outfile C4-4workers.txt
